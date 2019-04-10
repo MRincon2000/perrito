0e202134167f8e96c1585e0d1f38efae3b19317f
Estudiante <Estudiante@BE401S103C08ING.unal.loc>
2019-04-10 09:04:47 -0500

    crea el perrito

---
+++ src/Perrito.java
@@ -0,0 +1,28 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+/**
+ *
+ * @author Estudiante
+ */
+public class Perrito {
+    public String nombre;
+    public int edad;
+    public float peso;
+    public String color;
+    public void comer(){
+        
+    }
+    public void dormir(){
+        
+    }
+    public void correr(){
+        
+    }
+    public void ladrar(){
+        
+    }
+}
